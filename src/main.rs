c4cb638dd4331af18a5c83e9387428dd8adbc85b
suba <84484832+suba327777@users.noreply.github.com>
2023-10-29 16:45:20 +0900

    add: conver to camelcase

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+fn snake_to_camel(s: &str) -> String {
+    let mut result = String::new();
+    let mut capitalize_next = false;
+
+    for c in s.chars() {
+        if c == '_' {
+            capitalize_next = true;
+        } else {
+            result.push(if capitalize_next {
+                c.to_uppercase().next().unwrap()
+            } else {
+                c
+            });
+            capitalize_next = false;
+        }
+    }
+
+    result
+}
+
 fn main() {
-    println!("Hello, world!");
+    let test = "snake_case";
+    println!("{:?}", snake_to_camel(test));
 }
