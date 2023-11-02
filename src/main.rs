ae88419a7391cf1f0cf3e8876a6e62429f55758c
suba <84484832+suba327777@users.noreply.github.com>
2023-11-03 02:39:00 +0900

    add:convert to snakecase

--- src/main.rs
+++ src/main.rs
@@ -31,7 +31,26 @@ fn snake_to_camel(s: &str) -> String {
     }
     result
 }
-//ファイルの中身を１行ずつ読み込む関数
+
+fn camel_to_snake(s: &str) -> String {
+    let mut result = String::new();
+    let mut last_was_upper = false;
+
+    for c in s.chars() {
+        if c.is_uppercase() {
+            if !last_was_upper {
+                result.push('_');
+            }
+            result.push(c.to_lowercase().next().unwrap());
+            last_was_upper = true;
+        } else {
+            result.push(c);
+            last_was_upper = false
+        }
+    }
+    result
+}
+
 fn convert_file_content(state: ConvertArgs) {
     match read_to_string(&state.path) {
         Ok(content) => {
@@ -39,7 +58,7 @@ fn convert_file_content(state: ConvertArgs) {
                 .lines()
                 .map(|line| match state.case {
                     Case::Camel => snake_to_camel(line),
-                    Case::Snake => snake_to_camel(line),
+                    Case::Snake => camel_to_snake(line),
                 })
                 .collect::<Vec<String>>()
                 .join("\n");
